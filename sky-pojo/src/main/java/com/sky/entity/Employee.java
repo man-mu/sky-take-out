bbda1caf7ff2b25bbea3783607cf4c446fc266ef
cxl <1914009676@qq.com>
2026-01-30 15:33:43 +0800

    新增员工业务开发

--- sky-pojo/src/main/java/com/sky/entity/Employee.java
+++ sky-pojo/src/main/java/com/sky/entity/Employee.java
@@ -16,30 +16,29 @@ public class Employee implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
-    private Long id;
+    private Long id;              // 员工唯一标识符（主键ID）
 
-    private String username;
+    private String username;      // 员工登录用户名
 
-    private String name;
+    private String name;          // 员工姓名
 
-    private String password;
+    private String password;      // 员工登录密码（加密后）
 
-    private String phone;
+    private String phone;         // 员工手机号码
 
-    private String sex;
+    private String sex;           // 员工性别
 
-    private String idNumber;
+    private String idNumber;      // 员工身份证号码
 
-    private Integer status;
+    private Integer status;       // 账号状态（启用/禁用等）
 
-    //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime createTime;
+    private LocalDateTime createTime;  // 记录创建时间
 
-    //@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
-    private LocalDateTime updateTime;
+    private LocalDateTime updateTime;  // 记录最后更新时间
 
-    private Long createUser;
+    private Long createUser;      // 创建此员工记录的用户ID
 
-    private Long updateUser;
+    private Long updateUser;      // 最后修改此员工记录的用户ID
+
 
 }
--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.admin;
 
 import com.sky.constant.JwtClaimsConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
 import com.sky.properties.JwtProperties;
@@ -29,6 +30,7 @@ import java.util.Map;
 @RequestMapping("/admin/employee")
 public class EmployeeController {
 
+
     @Autowired
     private EmployeeService employeeService;
     @Autowired
@@ -38,7 +40,7 @@ public class EmployeeController {
      * 登录
      *
      * @param employeeLoginDTO
-     * @return
+     * @return Result
      */
     @PostMapping("/login")
     @ApiOperation("员工登录")
@@ -72,6 +74,19 @@ public class EmployeeController {
     @ApiOperation("员工退出")
     @PostMapping("/logout")
     public Result<String> logout() {
+        return Result.success();
+    }
+
+    /**
+     * 新增员工
+     * @param employeeDTO
+     * @return
+     */
+    @PostMapping
+    @ApiOperation("新增员工")
+    public Result save(@RequestBody EmployeeDTO employeeDTO){
+        log.info("新增员工，员工数据：{}",employeeDTO);
+        employeeService.save(employeeDTO);
         return Result.success();
     }
 
